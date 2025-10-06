1f3a11a0f8984542da9506b5cdbddef33a930adf
iannohad <iannoh.ad@gmail.com>
2025-10-06 18:09:59 +0900

    Delete vocabList.js

--- vocabList.js
+++
@@ -1,7 +0,0 @@
-export const vocabList = [
-  { problem: "Haus", answer: "house" },
-  { problem: "Baum", answer: "tree" },
-  { problem: "Wasser", answer: "water" },
-  { problem: "Buch", answer: "book" },
-  { problem: "Himmel", answer: "sky" }
-];
