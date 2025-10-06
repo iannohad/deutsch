1fcc0108a4a666b82785f54d6b90f0f44765cdd9
iannohad <iannoh.ad@gmail.com>
2025-10-06 10:08:29 +0900

    Added hint

---
+++ probList.js
@@ -0,0 +1,7 @@
+export const probList = [
+  { problem: "Ich kann nicht sehr gut ____.", hint: "I can't see well.", answer: "sehen" },
+  { problem: "____ ist sie!", hint: "Here she is", answer: "Hier" },
+  { problem: "War das ein ____?", hint: "Was that a no?", answer: "Nein" },
+  { problem: "____ du es noch?", hint: "Do you still have it?", answer: "Hast" },
+  { problem: "Das ist ____.", hint: "This is nothing.", answer: "nichts" }
+];
