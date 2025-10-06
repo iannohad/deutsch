1639e3cc8798cb55841c81cb98d3337dc81dc66f
iannohad <iannoh.ad@gmail.com>
2025-10-06 19:23:44 +0900

    Update problem set

--- probSets.js
+++ probSets.js
@@ -20,8 +20,6 @@ export const probSets = {
       hint: "그는 현재 바이마르에 살고 있습니다.", 
       answer: "Er wohnt jetzt in Weimar."
     },
-  ], 
-  set2: [
     { problem: "beruflich, was, machen, er", 
       hint: "그의 직업은 무엇입니까?", 
       answer: "Was macht er beruflich?"
@@ -34,12 +32,48 @@ export const probSets = {
       hint: "당신은 그녀를 압니까? 네, 저는 그녀를 압니다.", 
       answer: "Kennen Sie sie? Ja, ich kenne sie."
     },
-  ], 
-  set3: [
     { problem: "haben, das Buch, ja, 인칭대명사", 
       hint: "그는 그 책을 가지고 있나요? 네, 그는 그 책을 가지고 있어요.", 
       answer: "Hat er das Buch? Ja, er hat das Buch."
     },
+    { problem: "인칭대명사, geben, was, die Kinder", 
+      hint: "당신은 그 아이들에게 무엇을 줍니까?", 
+      answer: "Was geben Sie den Kindern?"
+    },
+    { problem: "der Schüler, lehren, der Lehrer", 
+      hint: "어느 교사가 한 명의 학생을 가르친다.", 
+      answer: "Ein Lehrer lehrt einen Schüler."
+    },
+    { problem: "dort, 인칭대명사, der Platz, draußen, nehmen", 
+      hint: "그들은 저기 바깥에 자리를 잡니?", 
+      answer: "Nehmen sie dort draußen Platz?"
+    },
+    { problem: "die Prüfung, haben, heute, 인칭대명사", 
+      hint: "너희는 오늘 시험이 하나 있니?", 
+      answer: "Habt ihr heute eine Prüfung?"
+    },
+    { problem: "nein, haben, kein, das Haus, 인칭대명사", 
+      hint: "그들은 집을 한 채 가지고 있습니까? 아니오, 그들은 집을 가지고 있지 않습니다.", 
+      answer: "Haben sie ein Haus? Nein, sie haben kein Haus."
+    },
+    { problem: "was, sein, 인칭대명사, von, Beruf, die Lehrerin", 
+      hint: "그녀의 직업은 무엇이죠? 그녀는 여교사입니다.", 
+      answer: "Was ist sie von Beruf? Sie ist Lehrerin."
+    },
+    { problem: "was, der Spaziergang, 인칭대명사, machen", 
+      hint: "그들은 뭘 하고 있죠? 그들은 산책을 하고 있습니다.", 
+      answer: "Was machen sie? Sie machen einen Spaziergang."
+    },
+    { problem: "kennen, 인칭대명사, das Kind, der Onkel", 
+      hint: "나는 그 아이의 삼촌 한 명을 알고 있다.", 
+      answer: "Ich kenne einen Onkel des Kindes."
+    },
+  ], 
+  set2: [
+    
+  ], 
+  set3: [
+    
   ],
   // { problem: "", 
   // hint: "", 
