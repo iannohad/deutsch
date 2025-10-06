046f584b8b52ce7861ae61fbf50fb6891f297a2a
iannohad <iannoh.ad@gmail.com>
2025-10-06 18:10:25 +0900

    Add problem sets

---
+++ probSets.js
@@ -0,0 +1,66 @@
+// export const probList = [
+//   { problem: "Ich kann nicht sehr gut ____.", hint: "I can't see well.", answer: "sehen" },
+//   { problem: "____ ist sie!", hint: "Here she is", answer: "Hier" },
+//   { problem: "War das ein ____?", hint: "Was that a no?", answer: "Nein" },
+//   { problem: "____ du es noch?", hint: "Do you still have it?", answer: "Hast" },
+//   { problem: "Das ist ____.", hint: "This is nothing.", answer: "nichts" }
+// ];
+
+export const probSets = {
+  set1: [
+    { problem: "kommen, woher, er", 
+      hint: "그는 어디 출신입니까?", 
+      answer: "Woher kommt er?"
+    },
+    { problem: "er, Frankfurt, eigentlich, aus, kommen", 
+      hint: "그는 원래 프푸 출신입니다.", 
+      answer: "Er kommt eigentlich aus Frankfurt."
+    },
+    { problem: "Weimar, in, wohnen, er, jetzt", 
+      hint: "그는 현재 바이마르에 살고 있습니다.", 
+      answer: "Er wohnt jetzt in Weimar."
+    },
+  ], 
+  set2: [
+    { problem: "beruflich, was, machen, er", 
+      hint: "그의 직업은 무엇입니까?", 
+      answer: "Was macht er beruflich?"
+    },
+    { problem: "heißen, wie, er", 
+      hint: "그의 이름은 무엇입니까?", 
+      answer: "Wie heißt er?"
+    },
+    { problem: "kennen, 인칭대명사, ja", 
+      hint: "당신은 그녀를 압니까? 네, 저는 그녀를 압니다.", 
+      answer: "Kennen Sie sie? Ja, ich kenne sie."
+    },
+  ], 
+  set3: [
+    { problem: "haben, das Buch, ja, 인칭대명사", 
+      hint: "그는 그 책을 가지고 있나요? 네, 그는 그 책을 가지고 있어요.", 
+      answer: "Hat er das Buch? Ja, er hat das Buch."
+    },
+  ],
+  // { problem: "", 
+  // hint: "", 
+  // answer: ""
+  // },
+  // { problem: "", 
+  // hint: "", 
+  // answer: ""
+  // },
+  // { problem: "", 
+  // hint: "", 
+  // answer: ""
+  // },
+  // { problem: "", 
+  // hint: "", 
+  // answer: ""
+  // },
+
+
+  // { problem: "", 
+  // hint: "", 
+  // answer: ""
+  // },
+};
