fc96eae675fe2cc4036a17d63f7948db3ac78b53
iannohad <iannoh.ad@gmail.com>
2025-10-06 19:49:43 +0900

    Update problem set-2

--- probSets.js
+++ probSets.js
@@ -8,66 +8,114 @@
 
 export const probSets = {
   set1: [
-    { problem: "kommen, woher, er", 
-      hint: "그는 어디 출신입니까?", 
+    { problem: "그는 어디 출신입니까?", 
+      hint: "kommen, woher, er", 
       answer: "Woher kommt er?"
     },
-    { problem: "er, Frankfurt, eigentlich, aus, kommen", 
-      hint: "그는 원래 프푸 출신입니다.", 
+    { problem: "그는 원래 프푸 출신입니다.", 
+      hint: "er, Frankfurt, eigentlich, aus, kommen", 
       answer: "Er kommt eigentlich aus Frankfurt."
     },
-    { problem: "Weimar, in, wohnen, er, jetzt", 
-      hint: "그는 현재 바이마르에 살고 있습니다.", 
+    { problem: "그는 현재 바이마르에 살고 있습니다.", 
+      hint: "Weimar, in, wohnen, er, jetzt", 
       answer: "Er wohnt jetzt in Weimar."
     },
-    { problem: "beruflich, was, machen, er", 
-      hint: "그의 직업은 무엇입니까?", 
+    { problem: "그의 직업은 무엇입니까?", 
+      hint: "beruflich, was, machen, er", 
       answer: "Was macht er beruflich?"
     },
-    { problem: "heißen, wie, er", 
-      hint: "그의 이름은 무엇입니까?", 
+    { problem: "그의 이름은 무엇입니까?", 
+      hint: "heißen, wie, er", 
       answer: "Wie heißt er?"
     },
-    { problem: "kennen, 인칭대명사, ja", 
-      hint: "당신은 그녀를 압니까? 네, 저는 그녀를 압니다.", 
+    { problem: "당신은 그녀를 압니까? 네, 저는 그녀를 압니다.", 
+      hint: "kennen, 인칭대명사, ja", 
       answer: "Kennen Sie sie? Ja, ich kenne sie."
     },
-    { problem: "haben, das Buch, ja, 인칭대명사", 
-      hint: "그는 그 책을 가지고 있나요? 네, 그는 그 책을 가지고 있어요.", 
+    { problem: "그는 그 책을 가지고 있나요? 네, 그는 그 책을 가지고 있어요.", 
+      hint: "haben, das Buch, ja, 인칭대명사", 
       answer: "Hat er das Buch? Ja, er hat das Buch."
     },
-    { problem: "인칭대명사, geben, was, die Kinder", 
-      hint: "당신은 그 아이들에게 무엇을 줍니까?", 
+    { problem: "당신은 그 아이들에게 무엇을 줍니까?", 
+      hint: "인칭대명사, geben, was, die Kinder", 
       answer: "Was geben Sie den Kindern?"
     },
-    { problem: "der Schüler, lehren, der Lehrer", 
-      hint: "어느 교사가 한 명의 학생을 가르친다.", 
+    { problem: "어느 교사가 한 명의 학생을 가르친다.", 
+      hint: "der Schüler, lehren, der Lehrer", 
       answer: "Ein Lehrer lehrt einen Schüler."
     },
-    { problem: "dort, 인칭대명사, der Platz, draußen, nehmen", 
-      hint: "그들은 저기 바깥에 자리를 잡니?", 
+    { problem: "그들은 저기 바깥에 자리를 잡니?", 
+      hint: "dort, 인칭대명사, der Platz, draußen, nehmen", 
       answer: "Nehmen sie dort draußen Platz?"
     },
-    { problem: "die Prüfung, haben, heute, 인칭대명사", 
-      hint: "너희는 오늘 시험이 하나 있니?", 
+    { problem: "너희는 오늘 시험이 하나 있니?", 
+      hint: "die Prüfung, haben, heute, 인칭대명사", 
       answer: "Habt ihr heute eine Prüfung?"
     },
-    { problem: "nein, haben, kein, das Haus, 인칭대명사", 
-      hint: "그들은 집을 한 채 가지고 있습니까? 아니오, 그들은 집을 가지고 있지 않습니다.", 
+    { problem: "그들은 집을 한 채 가지고 있습니까? 아니오, 그들은 집을 가지고 있지 않습니다.", 
+      hint: "nein, haben, kein, das Haus, 인칭대명사", 
       answer: "Haben sie ein Haus? Nein, sie haben kein Haus."
     },
-    { problem: "was, sein, 인칭대명사, von, Beruf, die Lehrerin", 
-      hint: "그녀의 직업은 무엇이죠? 그녀는 여교사입니다.", 
+    { problem: "그녀의 직업은 무엇이죠? 그녀는 교사입니다.", 
+      hint: "was, sein, 인칭대명사, von, Beruf, die Lehrerin", 
       answer: "Was ist sie von Beruf? Sie ist Lehrerin."
     },
-    { problem: "was, der Spaziergang, 인칭대명사, machen", 
-      hint: "그들은 뭘 하고 있죠? 그들은 산책을 하고 있습니다.", 
+    { problem: "그들은 뭘 하고 있죠? 그들은 산책을 하고 있습니다.", 
+      hint: "was, der Spaziergang, 인칭대명사, machen", 
       answer: "Was machen sie? Sie machen einen Spaziergang."
     },
-    { problem: "kennen, 인칭대명사, das Kind, der Onkel", 
-      hint: "나는 그 아이의 삼촌 한 명을 알고 있다.", 
+    { problem: "나는 그 아이의 삼촌 한 명을 알고 있다.", 
+      hint: "kennen, 인칭대명사, das Kind, der Onkel", 
       answer: "Ich kenne einen Onkel des Kindes."
     },
+    { problem: "그 여자의 아버지는 이름이 어떻게 되나요?", 
+      hint: "heißen, wie, der Vater, die Frau", 
+      answer: "Wie heißt der Vater der Frau?"
+    },
+    { problem: "너희는 어디에서 일하니? 나는 베를린에서 일해. 그녀는 함부르크에서 일해.", 
+      hint: "Berlin, arbeiten, in, Hamburg, 인칭대명사, wo", 
+      answer: "Wo arbeitet ihr? Ich arbeite in Berlin. Sie arbeitet in Hamburg."
+    },
+    { problem: "어느 대학생이 친구 한 명을 가졌다. 그 친구는 독일 출신이다.", 
+      hint: "kommen, der Student, aus, der Freund, Deutschland, haben", 
+      answer: "Ein Student hat einen Freund. Der Freund kommt aus Deutschland."
+    },
+    { problem: "그들은 나중에 온다. 그들은 카페 하나를 찾고 있어.", 
+      hint: "später, 인칭대명사, das Cafe, kommen, suchen", 
+      answer: "Sie kommen später. Sie suchen ein Cafe."
+    },
+    // { problem: "", 
+    //   hint: "", 
+    //   answer: ""
+    // },
+    // { problem: "", 
+    //   hint: "", 
+    //   answer: ""
+    // },
+    //     { problem: "", 
+    //   hint: "", 
+    //   answer: ""
+    // },
+    // { problem: "", 
+    //   hint: "", 
+    //   answer: ""
+    // },
+    // { problem: "", 
+    //   hint: "", 
+    //   answer: ""
+    // },
+    // { problem: "", 
+    //   hint: "", 
+    //   answer: ""
+    // },
+    // { problem: "", 
+    //   hint: "", 
+    //   answer: ""
+    // },
+    // { problem: "", 
+    //   hint: "", 
+    //   answer: ""
+    // },
   ], 
   set2: [
     
